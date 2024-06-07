40f7cf309272813dab1fb89e506874aaba33158e
jl-serrano <serranojohnlloyd@bpoc.co.jp>
2024-06-07 16:48:14 +0800

    project setup for vue js and tailwind

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,5 @@
-import './bootstrap';
+import { createApp } from 'vue';
+import App from '@/App.vue';
+import router from '@/services/vue-router/router';
+
+createApp(App).use(router).mount('#app');
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,30 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/js/app.js'],
             refresh: true,
         }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, './resources/js'),
+            '@components': resolve(__dirname, './resources/js/components'),
+            '@pages': resolve(__dirname, './resources/js/pages'),
+            '@composables': resolve(__dirname, './resources/js/composables'),
+            '@images': resolve(__dirname, './public/images'),
+        },
+    },
 });
