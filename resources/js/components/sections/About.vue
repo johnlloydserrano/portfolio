e7d15e249fb9f4087f7c1e41ec3415f417563552
jl-serrano <serranojohnlloyd@bpoc.co.jp>
2024-08-08 17:54:57 +0800

    Add: about component section UI

---
+++ resources/js/components/sections/About.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="about-wrapper">
+        <div class="container mx-auto">
+            <div class="bg-transparent p-12 text-white">
+                <div>
+                    <div class="text-center mb-6">
+                        <span>Explore the milestones and recognition I’ve achieved through my work</span>
+                        <h1 class="text-5xl pt-2 font-bold">Achievements</h1>
+                    </div>
+                    <div class="inline-block border border-1 rounded-full">
+                        <div class="flex items-center p-2 space-x-4">
+                            <div class="w-1/3">
+                                <div class="w-12 h-12 rounded-full bg-red-600 flex justify-center items-center">
+                                    <div class="w-6 h-6 bg-red-200 text-xs text-center pt-1 font-bold">T</div>
+                                </div>
+                            </div>
+                            <div class="w-full pr-4">
+                                <p class="text-sm">Employee Of The Year</p>
+                                <p class="text-xs whitespace-nowrap">HIPE Japan Inc. Awards 2022</p>
+                            </div>
+                        </div>
+                    </div> 
+                    <div class="mt-6 h-full flex flex-wrap space-y-6 justify-center">
+                        <p class="leading-relaxed text-lg">
+                            Joining HIPE Japan on November 17, 2021, I became a part of the Walklog team, working on backend and UI design. 
+                            Just a year later, at the 2022 Christmas party, I was honored with the Employee of the Year award.
+                        </p>
+                        <div class="text-center">
+                            <img class="employee-of-the-year" src="images/eoy.jpg" alt="hipe-employee-of-the-year-2022" />
+                            <p class="text-gray-600 text-sm pt-1 font-semibold">December 17, 2022</p>
+                        </div>
+                        <p class="leading-relaxed text-lg">
+                            When my journey began at HIPE Japan on November 17, 2021, I was introduced to the Walklog project. 
+                            Responsible for both the backend and UI design of the Walklog project, it was initially a very hard and frustrating experience. 
+                            For almost a month, I struggled as the sole handler of the Walklog project, with the language being new to me and no prior experience in Ruby and  Ruby on Rails. 
+                            However, with the help of other HIPE Japan members from different teams, I was able to execute the project successfully. 
+                            I dedicated myself to ensuring seamless functionality and an intuitive user experience. 
+                            To my surprise and immense gratitude, at HIPE Japan's 2022 Christmas party, I was awarded Employee of the Year. 
+                            This recognition, given within my first year at the company, underscored my commitment and the impact of my contributions to the team's success.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="bg-white">
+                <div class="flex items-center justify-center py-6">
+                    <div class="w-1/4 text-center">
+                        <h2 class="text-6xl font-bold">0</h2>
+                        <h3 class="text-lg font-semibold">Lorem, ipsum dolor.</h3>
+                    </div>
+                    <div class="w-1/4 text-center">
+                        <h2 class="text-6xl font-bold">0</h2>
+                        <h3 class="text-lg font-semibold">Lorem, ipsum dolor.</h3>
+                    </div>
+                    <div class="w-1/4 text-center">
+                        <h2 class="text-6xl font-bold">0</h2>
+                        <h3 class="text-lg font-semibold">Lorem, ipsum dolor.</h3>
+                    </div>
+                    <div class="w-1/4 text-center">
+                        <h2 class="text-6xl font-bold">0</h2>
+                        <h3 class="text-lg font-semibold">Lorem, ipsum dolor.</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
