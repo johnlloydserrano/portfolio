33024774f5036a7ccb7a91fdc7ef7347af5d18f7
jl-serrano <serranojohnlloyd@bpoc.co.jp>
2024-08-08 17:56:39 +0800

    Add: personal projects component section UI

---
+++ resources/js/components/sections/PersonalProject.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="personal-project-wrapper">
+        <div class="container mx-auto">
+            <div class="bg-transparent py-12 text-white">
+                <div class="flex items-center justify-center">
+                    <div class="text-center">
+                        <span>Explore the personal projects I've developed to learn and innovate over my 5 years of experience</span>
+                        <h1 class="text-5xl pt-2 font-bold">Personal Projects</h1>
+                    </div>
+                </div>
+                <div class="flex justify-center m-4 p-4">
+                    <div
+                        class="w-full mt-6 grid grid-cols-1 gap-x-6 p-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8 py-6"
+                    >
+                        <div class="group relative">
+                            <div class="bg-white relative flex flex-wrap">
+                                <img src="images/blog-uo-ux-img1.jpg" class="object-contain p-6 w-full object-center" />
+                            </div>
+                        </div>
+                        <div class="group relative">
+                            <div class="bg-white relative flex flex-wrap">
+                                <img src="images/blog-uo-ux-img1.jpg" class="object-contain p-6 w-full object-center" />
+                            </div>
+                        </div>
+                        <div class="group relative">
+                            <div class="bg-white relative flex flex-wrap">
+                                <img src="images/blog-uo-ux-img1.jpg" class="object-contain p-6 w-full object-center" />
+                            </div>
+                        </div>
+                        <div class="group relative">
+                            <div class="bg-white relative flex flex-wrap">
+                                <img src="images/blog-uo-ux-img1.jpg" class="object-contain p-6 w-full object-center" />
+                            </div>
+                        </div>
+                        <div class="group relative">
+                            <div class="bg-white relative flex flex-wrap">
+                                <img src="images/blog-uo-ux-img1.jpg" class="object-contain p-6 w-full object-center" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
