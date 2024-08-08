d8e0a906208118ad9f02620ee89b264a2d220b4f
jl-serrano <serranojohnlloyd@bpoc.co.jp>
2024-08-08 17:56:11 +0800

    Add: contact component section UI

---
+++ resources/js/components/sections/Contact.vue
@@ -0,0 +1,109 @@
+<template>
+    <div class="contact-wrapper">
+        <div class="container mx-auto">
+            <div class="bg-white py-12">
+                <div class="flex items-center justify-center">
+                    <div class="text-center">
+                        <h1 class="text-5xl font-bold">CONTACT ME</h1>
+                    </div>
+                </div>
+                <div class="flex justify-center m-4 p-4 gap-x-8">
+                    <div class="w-9/12 bg-red-400 p-12 rounded-3xl">
+                        <form action="">
+                            <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+                                <div>
+                                    <label for="first-name" class="block text-sm font-semibold leading-6 text-gray-900"
+                                        >Lorem ipsum dolor sit.</label
+                                    >
+                                    <div class="mt-2.5">
+                                        <input
+                                            type="text"
+                                            name="first-name"
+                                            id="first-name"
+                                            autocomplete="given-name"
+                                            class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                        />
+                                    </div>
+                                </div>
+                                <div>
+                                    <label for="last-name" class="block text-sm font-semibold leading-6 text-gray-900"
+                                        >Lorem ipsum dolor sit.</label
+                                    >
+                                    <div class="mt-2.5">
+                                        <input
+                                            type="text"
+                                            name="last-name"
+                                            id="last-name"
+                                            autocomplete="family-name"
+                                            class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                        />
+                                    </div>
+                                </div>
+                                <div>
+                                    <label for="first-name" class="block text-sm font-semibold leading-6 text-gray-900"
+                                        >Lorem ipsum dolor sit.</label
+                                    >
+                                    <div class="mt-2.5">
+                                        <input
+                                            type="text"
+                                            name="first-name"
+                                            id="first-name"
+                                            autocomplete="given-name"
+                                            class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                        />
+                                    </div>
+                                </div>
+                                <div>
+                                    <label for="last-name" class="block text-sm font-semibold leading-6 text-gray-900"
+                                        >Lorem ipsum dolor sit.</label
+                                    >
+                                    <div class="mt-2.5">
+                                        <input
+                                            type="text"
+                                            name="last-name"
+                                            id="last-name"
+                                            autocomplete="family-name"
+                                            class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                        />
+                                    </div>
+                                </div>
+                                <div class="sm:col-span-2">
+                                    <label for="company" class="block text-sm font-semibold leading-6 text-gray-900"
+                                        >Lorem ipsum dolor sit.</label
+                                    >
+                                    <div class="mt-2.5">
+                                        <textarea name="" id="" class="w-full"></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="flex justify-center mt-6">
+                                <button
+                                    type="submit"
+                                    class="w-1/2 rounded-md bg-red-800 px-3.5 py-2.5 text-center text-sm font-semibold text-white"
+                                >
+                                    Lorem ipsum dolor sit.
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="w-3/12 bg-red-500 p-6 rounded-3xl">
+                        <div class="flex flex-wrap">
+                            <div>
+                                <h3 class="text-3xl font-bold text-white">Lorem, ipsum dolor.</h3>
+                                <p class="text-sm text-gray-600 pt-4">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, esse! Temporibus cum ea
+                                    ducimus, eligendi cumque nulla architecto? Tenetur, perferendis.
+                                </p>
+                            </div>
+                            <div class="pt-6">
+                                <p>+1 2345 6789</p>
+                                <p>+1 2345 6789</p>
+                                <p>+1 2345 6789</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
