9bc40a71769f8391cdeb0cc4beda968962cf32e9
jl-serrano <serranojohnlloyd@bpoc.co.jp>
2024-08-08 17:57:36 +0800

    Add: Landing page index

---
+++ resources/js/pages/landing/Index.vue
@@ -0,0 +1,15 @@
+<script setup>
+    import Headline from '@components/sections/Headline.vue';
+    import About from '@components/sections/About.vue';
+    import Skill from '@components/sections/Skill.vue';
+    import PersonalProject from '@components/sections/PersonalProject.vue';
+    import Contact from '@components/sections/Contact.vue';
+</script>
+
+<template>
+    <Headline></Headline>
+    <About></About>
+    <Skill></Skill>
+    <PersonalProject></PersonalProject>
+    <Contact></Contact>
+</template>
