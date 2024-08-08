f93a27ceec48fc05f33468275410585d6fdd25ad
jl-serrano <serranojohnlloyd@bpoc.co.jp>
2024-08-08 17:55:18 +0800

    Add: headline component section UI

---
+++ resources/js/components/sections/Headline.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="headline-wrapper">
+        <div class="container mx-auto">
+            <div class="flex p-6">
+                <div class="w-1/2 p-12">
+                    <div class="flex items-center h-full">
+                        <div class="py-10">
+                            <div class="space-y-8 text-white">
+                                <h1 class="text-6xl font-black tracking-wide">John LLoyd Serrano</h1>
+                                <p class="text-xl">
+                                    I am a passionate programmer who finds joy and fulfillment in the journey of creating unique and engaging experiences. I continuously explore many programming languages and technologies, driven by a thirst for knowledge and innovation. Alongside my development work, I enjoy creating clear and helpful documentation, ensuring that my discoveries and projects are well-documented and easily accessible for future reference. This commitment to learning and sharing knowledge not only enhances my own skills but also contributes to the growth and efficiency of the teams I work with.
+                                </p>
+                            </div>
+
+                            <div class="mt-10">
+                                <button class="bg-red-200 py-3.5 px-8 rounded-full">Download CV</button>
+                                <span class="px-4 text-white">More About Me -></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-1/2 flex justify-center items-center p-6">
+                    <img class="h-3/5" src="images/serrano.png" alt="johnlloydserrano" />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
